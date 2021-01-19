cf4e031851cf5dbeb1f9aadf3711eb07c21af0ab
Lenar Gilyazov <lgilyazovg@gmail.com>
2021-01-19 18:28:24 +0300

    Initial Commit

---
+++ LGLogger/LGLogger.h
@@ -0,0 +1,18 @@
+//
+//  LGLogger.h
+//  LGLogger
+//
+//  Created by Ленар Гилязов on 19.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LGLogger.
+FOUNDATION_EXPORT double LGLoggerVersionNumber;
+
+//! Project version string for LGLogger.
+FOUNDATION_EXPORT const unsigned char LGLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LGLogger/PublicHeader.h>
+
+
